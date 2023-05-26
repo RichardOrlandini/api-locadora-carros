080e2484ffe1602a54a082fdabf3eb06695cfc64
RichardOrlandini <richardorlandini09@gmail.com>
2023-05-26 14:48:51 -0300

    controllers de modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,62 +4,111 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-class ModeloController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+class ModeloController extends Controller {
+
+    private $modelo;
+    
+    public function __construct(Modelo $modelo) {
+        $this->modelo = $modelo;
+    }
+    
+    public function index()  {
+   
+        $modelos = $this->modelo->all();
+        return response()->json($modelos, 200);
+    }
+   
+    public function store(Request $request) {
+
+        $request->validate($this->modelo->rules());
+
+        $image = $request->file('imagem');
+        $imagem_urn =  $image->store('imagens/modelos', 'public');
+
+       $modelo = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->lugares,
+       ]);
+        
+       return response()->json($modelo, 201);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    public function show($id) {
+    
+        $modelo = $this->modelo->find($id);
+
+        if ($modelo === null){
+            return response()->json(['erro' => 'Recurso pesquisado não existe'], 404);
+        }
+
+        return response()->json($modelo, 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+    public function update(Request $request,  $id)  {
+
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null){
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe.'], 404);
+        }
+
+        if ($request->method() === 'PATCH'){
+
+            $regrasDinamicas = array();
+
+            //aplicando regras apenas nos parâmetros da requisição.
+            foreach($modelo->rules() as $input => $regra){
+
+                if (array_key_exists($input, $request->all())){
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            $request->validate($regrasDinamicas);
+
+        } else {
+            $request->validate($modelo->rules());
+        }
+
+        //remove a img antiga, caso tenha uma nova img na request.
+        if($request->file('imagem')){
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+        
+        $image = $request->file('imagem');
+        $imagem_urn =  $image->store('imagens/modelos', 'public');
+
+        $modelo->update([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->lugares,
+        ]);
+
+        return response()->json($modelo, 200);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Modelo $modelo)
-    {
-        //
-    }
+    public function destroy($id) {
+    
+        $modelo = $this->modelo->find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Modelo $modelo)
-    {
-        //
-    }
+        if($modelo === null){
+            return response()->json(['erro' => 'Impossível realizar a exclusão. O recurso solicitado não existe.'], 404);
+        }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Modelo $modelo)
-    {
-        //
-    }
+        //remove a img.
+        Storage::disk('public')->delete($modelo->imagem);
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Modelo $modelo)
-    {
-        //
+        $modelo->delete();
+        return response()->json(['msg' => 'O modelo foi removida com sucesso!'], 200);
     }
 }
--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'marca_id',
+        'nome',
+        'imagem',
+        'numero_portas',
+        'lugares',
+        'air_bag',
+        'abs'
+    ];
+
+    public function rules() {
+
+        return  [
+            'marca_id' => 'exists:marcas,id',
+            'nome' => 'required|unique:modelos,nome,'.$this->id.'|min:3',
+            'imagem' => 'required|file|mimes:png,jpeg,jpg',
+            'numero_portas' => 'required|integer|digits_between:1,5',
+            'lugares' => 'required|integer|digits_between:1,20',
+            'air_bag' => 'required|boolean',
+            'abs' => 'required|boolean',
+        ];
+        //Unique ignora o id no 3ª parâmetro.
+       
+    }
+
+    public function feedback() {
+
+        return [
+            'required' => 'O campo :attribute é obrigatório.',
+            'imagem.mimes' => 'O arquivo deve ser apenas do tipo PNG,JPEG,JPG.',
+            'nome.unique' => 'O nome já existe.',
+            'nome.min' => 'O nome deve ter no mínimo 3 caracteres.',
+        ];
+    }
 }
